e09a25137c6842918c2da06b6f5a2e30df0f091f
Pete Kubiak <peter.s.kubiak@bath.edu>
2026-05-03 20:51:45 +0100

    Write simple first pass of using light sensing listeners

--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,31 @@
 #include "MicroBit.h"
-#include "samples/Tests.h"
+// #include "samples/Tests.h"
 
 MicroBit uBit;
+
+static void onLight(MicroBitEvent)
+{
+    uBit.serial.printf("BRIGHT LIGHT!");
+}
+
+static void onDark(MicroBitEvent)
+{
+    uBit.serial.printf("Hey who turned out the lights?");
+}
 
 int main()
 {
     uBit.init();
 
-    out_of_box_experience();
+    uBit.display.setDisplayMode(DisplayMode::DISPLAY_MODE_BLACK_AND_WHITE_LIGHT_SENSE);
+
+    uBit.messageBus.listen(MICROBIT_ID_DISPLAY, MICROBIT_DISPLAY_EVT_LIGHT_LEVEL_HIGH, onLight);
+    uBit.messageBus.listen(MICROBIT_ID_DISPLAY, MICROBIT_DISPLAY_EVT_LIGHT_LEVEL_DARK, onDark);
+
+    while(1)
+    {
+        uBit.sleep(1000);
+    }
 
     microbit_panic( 999 );
 }
